249636d19da9aff9fe89190f617b8873322beb0f
Gavin J <65034058+GavinJohnston@users.noreply.github.com>
2022-06-13 23:05:15 +0100

    working on identity

--- Blogger/Program.cs
+++ Blogger/Program.cs
@@ -1,27 +1,35 @@
 using Blogger.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Blogger.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("BloggerIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'BloggerIdentityDbContextConnection' not found.");
 
+builder.Services.AddDbContext<BloggerIdentityDbContext>(options =>
+    options.UseSqlServer(connectionString)); 
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<BloggerIdentityDbContext>();;
+
 builder.Services.AddControllers().AddNewtonsoftJson();
 
-builder.Services.AddDbContext<BloggerContext>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection"));
-});
 
-builder.Services.AddDefaultIdentity<IdentityUser>
-    (options =>
-    {
-        options.SignIn.RequireConfirmedAccount = false;
-        options.Password.RequireDigit = false;
-        options.Password.RequiredLength = 6;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequireLowercase = false;
-    })
-.AddEntityFrameworkStores<BloggerContext>();
+//builder.Services.AddDbContext<BloggerContext>(options => {
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("connectionString"));
+//});
+
+//builder.Services.AddDefaultIdentity<IdentityUser>
+//    (options =>
+//    {
+//        options.SignIn.RequireConfirmedAccount = false;
+//        options.Password.RequireDigit = false;
+//        options.Password.RequiredLength = 6;
+//        options.Password.RequireNonAlphanumeric = false;
+//        options.Password.RequireUppercase = false;
+//        options.Password.RequireLowercase = false;
+//    })
+//.AddEntityFrameworkStores<BloggerContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
